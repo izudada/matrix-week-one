ab44ba7c8cdd6dc2690f85fa6d6563530486b080
Udeagbala Anthony <tonyudeagbala@gmail.com>
2021-06-07 14:20:57 +0000

    Finnaly completed task3

--- src/task3.py
+++ src/task3.py
@@ -1,26 +1,44 @@
 def dictComp(stop, step):
     dict_list = {}
-    count = 1
-    new_count = 0
+    count = 0
+
 
     array_list = [ array for array in range(1, stop+1) ]
     
     for digit in array_list:
-        
+        # Condition runs only on the first iteration when dict_list has no key value pair
         if len(dict_list) == 0:
-            dict_list["items-{}".format(count)] = [digit,]
-            
-        elif len(dict_list.keys()[-1]) < step:
-            last_key = dict_list.keys()[-1]
-            dict_list[last_key].append([digit])
+            count += 1
+            dict_list["items-{}".format(count)] = [digit]
 
+        #   Condition when each value as a list is not greater than step 
+        elif len(dict_list["items-{}".format(count)]) < step :
+            dict_list["items-{}".format(count)].append(digit)
+        
+        #   Condition for creating new key value pair after the lenth of last pair is equal to step
+        else:
+            count += 1
+            dict_list["items-{}".format(count )] = [digit]
 
-    print(len(dict_list.keys()[-1]))
+    if len(dict_list["items-{}".format(count)]) < step:
+        dict_list.popitem()
+
     print(dict_list)
 
 
-stop = int(input("Enter a number as the limit for your array/list: "))
-step = int(input("Enter the digit to use as a step"))
+try:
+    stop = int(input("Enter a number as the stop limit for your array/list: "))
+    step = int(input("Enter the digit to use as a step: "))
+    print("\n")
 
+    dictComp(stop, step)
 
-dictComp(stop, step)
+except ValueError:
+    print("\n")
+    print("Invalid input, please enter only a number")
+    print("\n")
+    stop = int(input("Enter a number as the stop limit for your array/list: "))
+    step = int(input("Enter the digit to use as a step: "))
+    print("\n")
+
+    dictComp(stop, step)
